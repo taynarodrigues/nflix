a4ba06bb2bfa68a5c6e1c50155dcd8c9676ecfcc
Tayna Rodrigues <taynajesus@gmail.com>
2020-06-15 16:56:49 -0400

    Validando chaves do Local Storage

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -3,11 +3,14 @@ Quando("eu faço login com {string} e {string}") do |email, password|
   find("#emailId").set email 
   find("#passId").set password
   click_button "Entrar"
-  sleep 5
+  # sleep 5
 end
 
 Então("devo ser autenticado") do
-  pending # Write code here that turns the phrase above into concrete actions
+  js_script = 'return window.localStorage.getItem("default_auth_token");'
+  token = page.execute_script(js_script)
+  # puts token
+   expect(token.length).to be 147 # verificar o token
 end
 
 Então("devo ver {string} na área logada") do |string|

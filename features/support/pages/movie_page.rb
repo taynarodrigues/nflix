518fdd5ba5817bc2f17b2af7a872540cf9b2bc5a
Tayna Rodrigues <taynajesus@gmail.com>
2020-06-19 08:30:05 -0400

    preenchendo formulário - ator

--- features/support/pages/movie_page.rb
+++ features/support/pages/movie_page.rb
@@ -10,5 +10,14 @@ class MoviePage
 
       find("input[placeholder=Status]").click
       find(".el-select-dropdown__item", text: movie["status"]).click
+
+      find("input[name=year]").set movie["year"]
+      find("input[name=release_date]").set movie["release_date"]
+
+      actor = find(".input-new-tag")
+      movie["cast"].each do |a|
+        actor.set a
+        actor.send_keys :tab
+      end
     end
 end
